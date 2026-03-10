11add7a8282db89a1d4d20a486a8a28a4a0e528b
AliAbdur1 <AliAbdur171@gmail.com>
2026-03-10 00:41:54 -0400

    dropping ballz

--- src/components/PhysicsExample.jsx
+++ src/components/PhysicsExample.jsx
@@ -23,6 +23,18 @@ function Example() {
     });
     gui.close();
 
+    const debugObject = {
+      createSphere: () => {
+        createSphere(Math.random() * 0.5, {
+          x: (Math.random() - 0.5) * 3,
+          y: 3,
+          z: (Math.random() - 0.5) * 3
+        })
+      }
+    }
+    
+    gui.add(debugObject, 'createSphere')
+
     const toggleGUI = (event) => {
       if (event.key === 'h') {
         gui.show(gui._hidden); // toggle debug UI
@@ -78,16 +90,7 @@ const defaultContactMaterial = new CANNON.ContactMaterial(
 world.addContactMaterial(defaultContactMaterial)
 world.defaultContactMaterial = defaultContactMaterial
 
-// World Sphere
-const sphereShape = new CANNON.Sphere(0.5)
-const sphereBody = new CANNON.Body({
-    mass: 1,
-    position: new CANNON.Vec3(0, 3, 0),
-    shape: sphereShape,
-    //material: defaultMaterial //not needed here because of 'world.defaultContactMaterial', but --> contact material property instructions
-})
-sphereBody.applyLocalForce(new CANNON.Vec3(150, 0, 0), new CANNON.Vec3(0,0,0))
-world.addBody(sphereBody)
+
 
 //Floor
 const floorShape = new CANNON.Plane()
@@ -104,22 +107,7 @@ world.addBody(floorBody)
 //World end
 //Physics end
 
-/**
- * Test sphere
- */
-const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 32, 32),
-    new THREE.MeshStandardMaterial({
-        metalness: 0.3,
-        roughness: 0.4,
-        envMap: environmentMapTexture,
-        envMapIntensity: 0.5
-        
-    })
-)
-sphere.castShadow = true
-sphere.position.y = 0.5
-scene.add(sphere)
+
 
 /**
  * Floor
@@ -205,6 +193,45 @@ renderer.shadowMap.type = THREE.PCFSoftShadowMap;
     };
     window.addEventListener('dblclick', handleDblClick);
 
+    /**
+     * Utilis
+     */
+    const objectsToUpdate = []
+
+    const sphereGeometry = new THREE.SphereGeometry(1, 20, 20)
+    const sphereMaterial = new THREE.MeshStandardMaterial({
+          metalness: 0.3,
+          roughness: 0.4,
+          envMap: environmentMapTexture
+        })
+
+    const createSphere = (radius, position) => {
+      // THREE.js mesh
+      const mesh = new THREE.Mesh(sphereGeometry,sphereMaterial)
+      mesh.scale.set(radius, radius, radius)
+      mesh.castShadow = true
+      mesh.position.copy(position)
+      scene.add(mesh)
+
+      // CANNON.js body
+      const shape = new CANNON.Sphere(radius)
+      const body = new CANNON.Body({
+        mass: 1,
+        position: new CANNON.Vec3(0, 3, 0),
+        shape,
+        material: defaultMaterial
+      })
+      body.position.copy(position)
+      world.addBody(body)    
+
+      objectsToUpdate.push({
+        mesh,
+        body
+      })
+    }
+
+    createSphere(0.5, { x: 0, y: 3, z: 0 })
+
     // Animation loop
     const clock = new THREE.Clock();
     let oldElapsedTime = 0;
@@ -214,10 +241,15 @@ renderer.shadowMap.type = THREE.PCFSoftShadowMap;
       oldElapsedTime = elapsedTime;
 
       // Update physics world
-      sphereBody.applyForce(new CANNON.Vec3(-0.5, 0, 0), sphereBody.position) // 'wind' force on the ball
+      
       world.step(1 / 60, deltaTime, 3)
 
-      sphere.position.copy(sphereBody.position)
+      for(const object of objectsToUpdate) {
+        object.mesh.position.copy(object.body.position)
+
+      }
+
+      
 
       // Example animation
     //   cube.rotation.y = elapsedTime;
